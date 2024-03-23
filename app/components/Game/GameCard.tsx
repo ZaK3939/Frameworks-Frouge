90f38a70a24c5dff65fc8d32e450d280b58b667b
libdefi <libcreatesweb3@gmail.com>
2024-03-23 21:42:20 +0800

    Update card colors

--- app/components/Game/GameCard.tsx
+++ app/components/Game/GameCard.tsx
@@ -4,17 +4,45 @@ export interface GameCardProps {
   title: string;
   imageUrl: string;
   attributes: string[];
+  category: string;
 }
 
 export const GameCard: React.FC<GameCardProps> = ({
   title,
   imageUrl,
   attributes,
+  category,
 }) => {
+  // Determine background and border colors based on the category
+  let backgroundColor, borderColor;
+  switch (category) {
+    case "enemy":
+      backgroundColor = "#7D0202";
+      borderColor = "#FF0000";
+      break;
+    case "equipment":
+      backgroundColor = "#CC8207";
+      borderColor = "#B86900";
+      break;
+    case "item":
+      backgroundColor = "#031159";
+      borderColor = "#001D85";
+      break;
+    default:
+      backgroundColor = "#CC8207";
+      borderColor = "#B86900";
+  }
+
   return (
-    <div tw="flex flex-col items-center bg-[#CC8207] rounded-md text-white w-[110px] h-[120px] border border-white text-xs text-center">
+    <div
+      tw="flex flex-col items-center rounded-md text-white w-[110px] h-[120px] text-xs text-center"
+      style={{
+        backgroundColor: backgroundColor,
+        border: `1px solid ${borderColor}`,
+      }}
+    >
       <p tw="flex items-center justify-center h-6 m-0">{title}</p>
-      <div tw="bg-[#B86900] w-full flex justify-center">
+      <div tw="w-full flex justify-center" style={{ backgroundColor: borderColor }}>
         <img
           src={`${process.env.NEXT_PUBLIC_URL}/card-images/${imageUrl}`}
           width={40}
@@ -23,8 +51,8 @@ export const GameCard: React.FC<GameCardProps> = ({
         />
       </div>
       {attributes.map((attr, index) => (
-        <div tw="px-1 flex items-left w-full">
-          <span key={index}>{attr}</span>
+        <div tw="px-1 flex items-left w-full" key={index}>
+          <span>{attr}</span>
         </div>
       ))}
     </div>
