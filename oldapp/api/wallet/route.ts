7f7b0dd16720ab2611ecb7974377d326f6ed728a
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-03-23 12:28:32 +0900

    fix: frams lib

--- oldapp/api/wallet/route.ts
+++ oldapp/api/wallet/route.ts
@@ -1,30 +1,40 @@
-import { errorFrame, parseFrameRequest, getOwnerAddressFromFid, successFrame, createWalletFrame } from '@/lib/farcaster';
-import { FrameRequest } from '@coinbase/onchainkit';
-import { NextRequest, NextResponse } from 'next/server';
-import { createOrFindEmbeddedWalletForFid } from '@/lib/embedded-wallet';
+import {
+  errorFrame,
+  parseFrameRequest,
+  getOwnerAddressFromFid,
+  successFrame,
+  createWalletFrame,
+} from "@/app/lib/farcaster";
+import { FrameRequest } from "@coinbase/onchainkit";
+import { NextRequest, NextResponse } from "next/server";
+import { createOrFindEmbeddedWalletForFid } from "@/app/lib/embedded-wallet";
 
 export async function POST(req: NextRequest): Promise<Response> {
-    let frameRequest: FrameRequest | undefined;
+  let frameRequest: FrameRequest | undefined;
 
-    // Parse and validate request from Frame for fid
-    try {
-        frameRequest = await req.json();
-        if (!frameRequest) throw new Error('Could not deserialize request from frame');
-    } catch {
-        return new NextResponse(errorFrame);
-    }
-    const {fid, isValid} = await parseFrameRequest(frameRequest);
-    if (!fid || !isValid) return new NextResponse(errorFrame);
+  // Parse and validate request from Frame for fid
+  try {
+    frameRequest = await req.json();
+    if (!frameRequest)
+      throw new Error("Could not deserialize request from frame");
+  } catch {
+    return new NextResponse(errorFrame);
+  }
+  const { fid, isValid } = await parseFrameRequest(frameRequest);
+  if (!fid || !isValid) return new NextResponse(errorFrame);
 
-    // Query FC Registry contract to get owner address from fid
-    const ownerAddress = await getOwnerAddressFromFid(fid);
-    if (!ownerAddress) return new NextResponse(errorFrame);
+  // Query FC Registry contract to get owner address from fid
+  const ownerAddress = await getOwnerAddressFromFid(fid);
+  if (!ownerAddress) return new NextResponse(errorFrame);
 
-    // Generate an embedded wallet associated with the fid
-    const embeddedWalletAddress = await createOrFindEmbeddedWalletForFid(fid, ownerAddress);
-    if (!embeddedWalletAddress) return new NextResponse(errorFrame);
+  // Generate an embedded wallet associated with the fid
+  const embeddedWalletAddress = await createOrFindEmbeddedWalletForFid(
+    fid,
+    ownerAddress,
+  );
+  if (!embeddedWalletAddress) return new NextResponse(errorFrame);
 
-    return new NextResponse(createWalletFrame(embeddedWalletAddress));
+  return new NextResponse(createWalletFrame(embeddedWalletAddress));
 }
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
--- oldapp/page.tsx
+++ oldapp/page.tsx
@@ -1,20 +1,20 @@
-import {getFrameMetadata} from '@coinbase/onchainkit';
-import type {Metadata} from 'next';
-import {FrameImageUrls, FRAME_BASE_URL} from '../lib/farcaster';
-import RedirectToDemo from '@/components/redirect';
+import { getFrameMetadata } from "@coinbase/onchainkit";
+import type { Metadata } from "next";
+import { FrameImageUrls, FRAME_BASE_URL } from "../app/lib/farcaster";
+import RedirectToDemo from "@/components/redirect";
 
 const frameMetadata = getFrameMetadata({
-  buttons: ['Create a wallet'],
+  // buttons: ["Create a wallet"],
   image: FrameImageUrls.START,
   post_url: `${FRAME_BASE_URL}/api/wallet`,
 });
 
 export const metadata: Metadata = {
-  title: 'Privy Frames',
-  description: 'Privy Frames',
+  title: "Privy Frames",
+  description: "Privy Frames",
   openGraph: {
-    title: 'Privy Frames',
-    description: 'Privy Frames',
+    title: "Privy Frames",
+    description: "Privy Frames",
     images: [FrameImageUrls.START],
   },
   other: {
@@ -22,11 +22,11 @@ export const metadata: Metadata = {
   },
 };
 
-
 export default function Page() {
   return (
     <>
       <h1>Privy Frames</h1>
       <RedirectToDemo />
-    </>);
+    </>
+  );
 }
