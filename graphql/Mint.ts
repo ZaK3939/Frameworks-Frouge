45d362559c76558a5b3d51c54aaf008022364165
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-03-24 21:34:48 +0900

    feat: update

--- graphql/Mint.ts
+++ graphql/Mint.ts
@@ -1,19 +1,25 @@
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client/core"
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client/core";
 
-const AIRSTACK_ENDPOINT = "https://api.airstack.xyz/gql"
-const AIRSTACK_API_KEY = process.env.AIRSTACK_API_KEY
+const AIRSTACK_ENDPOINT = "https://api.airstack.xyz/gql";
+const AIRSTACK_API_KEY = process.env.AIRSTACK_API_KEY;
 
 const client = new ApolloClient({
-    uri: AIRSTACK_ENDPOINT,
-    cache: new InMemoryCache(),
-    headers: { Authorization: AIRSTACK_API_KEY || "" },
-})
+  uri: AIRSTACK_ENDPOINT,
+  cache: new InMemoryCache(),
+  headers: { Authorization: AIRSTACK_API_KEY || "" },
+});
 
 export async function AllHolders(): Promise<any> {
   const query = gql`
     query TokenHoldersFrogueNFT {
       Base: TokenBalances(
-        input: {filter: {tokenAddress: {_eq: "0x436299b0E33BF08b8914034A2455Fd4aEBC0dd31"}}, blockchain: base, limit: 200}
+        input: {
+          filter: {
+            tokenAddress: { _eq: "0x250ABA37496C5dFb2AE3D75176f98c9cbB0394E3" }
+          }
+          blockchain: base
+          limit: 200
+        }
       ) {
         TokenBalance {
           owner {
@@ -23,30 +29,29 @@ export async function AllHolders(): Promise<any> {
         }
       }
     }
-  `
-  
+  `;
+
   const response = await client.query({
-      query
-  })
-  return response.data
+    query,
+  });
+  return response.data;
 }
 
-
 export async function TokenBalance(address: string): Promise<any> {
-    const query = gql`
+  const query = gql`
       query FarcasterAccount {
         Base: TokenBalances(
-          input: {filter: {owner: {_eq: "${address}"}, tokenAddress: {_eq: "0x436299b0E33BF08b8914034A2455Fd4aEBC0dd31"}}, blockchain: base, limit: 200}
+          input: {filter: {owner: {_eq: "${address}"}, tokenAddress: {_eq: "0x250ABA37496C5dFb2AE3D75176f98c9cbB0394E3"}}, blockchain: base, limit: 200}
         ) {
           TokenBalance {
             amount
           }
         }
       }
-    `
-    
-    const response = await client.query({
-        query
-    })
-    return response.data
+    `;
+
+  const response = await client.query({
+    query,
+  });
+  return response.data;
 }
